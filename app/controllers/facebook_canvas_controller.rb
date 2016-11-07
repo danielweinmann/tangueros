0c02dffd43969588cfcf517cc65b04db3702af11
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-07 18:23:02 -0200

    Removes facebook canvas layout

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -2,10 +2,15 @@ class FacebookCanvasController < ApplicationController
   before_action :authenticate_user!, except: %i[index]
   after_action :verify_authorized, except: %i[index]
   after_action :verify_policy_scoped, only: %i[]
+  after_action :set_frame_header
   skip_before_action :verify_authenticity_token
-  layout "application"
 
   def index
+  end
+
+  private
+
+  def set_frame_header
     response.headers["X-Frame-Options"] = "GOFORIT"
   end
 end
