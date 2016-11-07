d69fbf8c23bb31edf3e55394e959de8c0d4921e2
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-07 01:09:43 -0200

    Adds about and credits pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, unless: -> {devise_controller? || self.class == HighVoltage::PagesController}
   after_action :verify_policy_scoped, unless: -> {devise_controller? || self.class == HighVoltage::PagesController}
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  before_action :check_profile, unless: -> {devise_controller? || action_name == 'profile_image' || action_name == 'update_profile_image' || action_name == 'location' || action_name == 'update_location' || action_name == 'roles' || action_name == 'update_roles' }
+  before_action :check_profile, unless: -> {devise_controller? || self.class == HighVoltage::PagesController || action_name == 'profile_image' || action_name == 'update_profile_image' || action_name == 'location' || action_name == 'update_location' || action_name == 'roles' || action_name == 'update_roles' }
 
   def namespace
     names = self.class.to_s.split('::')
