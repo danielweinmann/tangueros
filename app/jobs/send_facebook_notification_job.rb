f9f1d5ae6ea7faed96ef070a49ec49f1e5991461
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-07 18:04:43 -0200

    Adds facebook example code to test canvas

--- app/jobs/send_facebook_notification_job.rb
+++ app/jobs/send_facebook_notification_job.rb
@@ -6,6 +6,6 @@ class SendFacebookNotificationJob < ApplicationJob
     user = notification.user
     return unless user.provider == 'facebook' && user.uid.present?
     graph = Koala::Facebook::API.new(Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']).get_app_access_token)
-    response = graph.put_connections(user.uid, "notifications", template: notification.facebook_content, href: "")
+    response = graph.put_connections(user.uid, "notifications", template: notification.facebook_content)
   end
 end
