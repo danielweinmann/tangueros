7f40c225a5442f9c2d0c269a53b276c9db9f85af
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-07 23:04:26 -0200

    Adds facebook_canvas#show

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -1,10 +1,15 @@
 class FacebookCanvasController < ApplicationController
-  before_action :authenticate_user!, except: %i[index]
-  after_action :verify_authorized, except: %i[index]
+  before_action :authenticate_user!, except: %i[index show]
+  after_action :verify_authorized, except: %i[index show]
   after_action :verify_policy_scoped, only: %i[]
   skip_before_action :verify_authenticity_token
 
   def index
     redirect_to :root
+  end
+
+  def show
+    return redirect_to notifications_path if current_user
+    @notification = Notification.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
     collection do
       post :index
     end
+    member do
+      post :show
+    end
   end
   root "users#index"
 end
