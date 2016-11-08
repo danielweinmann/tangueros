eab962e9883ee566f1740995621359f3cd2f3f0f
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-08 14:27:07 -0200

    Does not use layout on welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+  layout false
   def welcome(user)
     @user = user
     mail(to: @user.to_email, from: '"Tangueros" <hello@tangueros.club>', subject: 'Welcome to Tangueros')
