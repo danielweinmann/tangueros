c0b20f0dd5829db3cbba9bad8c0815fae97f5c0c
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-08 14:37:37 -0200

    Changes back to html welcome email, since SendGrid already uses html

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ApplicationMailer
-  layout false
   def welcome(user)
     @user = user
     mail(to: @user.to_email, from: '"Tangueros" <hello@tangueros.club>', subject: 'Welcome to Tangueros')
