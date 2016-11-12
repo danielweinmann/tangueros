457f70fba62549f3ba97d62e007d38b3cc644c14
Daniel Weinmann <danielweinmann@gmail.com>
2016-11-12 01:23:40 -0200

    Enables multiple dismissals of the same user so we can show buttons on profile even if user has been dismissed

--- app/controllers/dismissals_controller.rb
+++ app/controllers/dismissals_controller.rb
@@ -4,7 +4,12 @@ class DismissalsController < ApplicationController
   after_action :verify_policy_scoped, only: %i[]
 
   def create
-    @dismissal = Dismissal.new(dismissal_params)
+    @dismissal = Dismissal.where(user: current_user, dismissed_user: dismissal_params[:dismissed_user_id]).first
+    if @dismissal
+      @dismissal.updated_at = Time.now
+    else
+      @dismissal = Dismissal.new(dismissal_params)
+    end
     @dismissal.user = current_user
     authorize @dismissal
 
