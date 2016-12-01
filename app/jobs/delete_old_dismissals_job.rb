de5872ef8cb2ab1877a9a45592ca20996e0d4dba
Daniel Weinmann <danielweinmann@gmail.com>
2016-12-01 11:58:18 -0200

    Changes delete old dismissals job to 28 days

--- app/jobs/delete_old_dismissals_job.rb
+++ app/jobs/delete_old_dismissals_job.rb
@@ -2,6 +2,6 @@ class DeleteOldDismissalsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Dismissal.where("updated_at < (now() - interval '7 days')").delete_all
+    Dismissal.where("updated_at < (now() - interval '28 days')").delete_all
   end
 end
